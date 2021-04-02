900fe75f33bf87749f52a8a4890eabad605c9327
Vishnu <vishnupothan2@gmail.com>
2021-04-02 11:17:14 +0530

    Home spinner and profile card updated

--- app/src/main/java/com/group2/attendancestudentapp/HomeActivity.java
+++ app/src/main/java/com/group2/attendancestudentapp/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.group2.attendancestudentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-
+    AutoCompleteTextView editTextFilledExposedDropdown;
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +29,26 @@ public class HomeActivity extends AppCompatActivity {
             switch (itemId) {
                 case R.id.home:
                     startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                    overridePendingTransition(0,0);
+                    overridePendingTransition(0, 0);
                     return true;
                 case R.id.profile:
                     startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                    overridePendingTransition(0,0);
+                    overridePendingTransition(0, 0);
                     return true;
                 case R.id.message:
                     startActivity(new Intent(getApplicationContext(), MessageActivity.class));
-                    overridePendingTransition(0,0);
+                    overridePendingTransition(0, 0);
                     return true;
             }
             finish();
             return false;
         });
+
+        // Attendance View Spinner initialize
+        String[] attendanceViewType = new String[]{"By Percentage", "By Date", "By Subject"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, attendanceViewType);
+        editTextFilledExposedDropdown = findViewById(R.id.attendanceViewText);
+        editTextFilledExposedDropdown.setAdapter(adapter);
 
         // TODO Badge code for icon
         /*BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.home);
--- app/src/main/java/com/group2/attendancestudentapp/MessageActivity.java
+++ app/src/main/java/com/group2/attendancestudentapp/MessageActivity.java
@@ -1,7 +1,6 @@
 package com.group2.attendancestudentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
