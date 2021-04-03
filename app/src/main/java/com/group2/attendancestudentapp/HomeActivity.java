33182edd3e97c8a53685dffe81c5ab3f1389c461
Vishnu <vishnupothan2@gmail.com>
2021-04-03 11:26:01 +0530

    problem solved

--- app/src/main/java/com/group2/attendancestudentapp/HomeActivity.java
+++ app/src/main/java/com/group2/attendancestudentapp/HomeActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -17,7 +14,6 @@ public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     AutoCompleteTextView editTextFilledExposedDropdown;
-
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +49,6 @@ public class HomeActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, attendanceViewType);
         editTextFilledExposedDropdown = findViewById(R.id.attendanceViewText);
         editTextFilledExposedDropdown.setAdapter(adapter);
-
-        editTextFilledExposedDropdown.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-            }
-        });
 
         // TODO Badge code for icon
         /*BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.home);
